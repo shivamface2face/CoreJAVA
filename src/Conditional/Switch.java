2e2580ec157cdb5af541a77c6ab8f135d8710a27
shivamface2face <shivamfaceuit@gmail.com>
2022-08-03 12:32:15 +0530

    switch

---
+++ src/Conditional/Switch.java
@@ -0,0 +1,22 @@
+package Conditional;
+
+import java.util.Scanner;
+
+public class Switch {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int button= in.nextInt();
+
+        switch (button){
+            case 1:
+                System.out.println("print 1");
+            break;
+            case 2:
+                System.out.println("print 2");
+                break;
+            default:
+                System.out.println("print none");
+
+        }
+    }
+}
