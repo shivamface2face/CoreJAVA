fe97da6f0d06185de330addd7db73d611eb17689
shivamface2face <shivamfaceuit@gmail.com>
2022-08-04 10:47:01 +0530

    pattern pyramid

--- src/Pattern/Basic.java
+++ src/Pattern/Basic.java
@@ -63,6 +63,27 @@ public class Basic {
             }
             System.out.println();
         }
+        System.out.println();
+        System.out.println();
+
+//   5 invert pyramid
+//            *
+//          * *
+//        * * *
+
+        for (int i = 1; i <=n; i++) {
+//             1st inner loop for space
+            for (int j = 1; j <=n-i; j++) {
+                System.out.print(" ");
+            }
+//            2nd inner loop for start as usual as ragular pyramid
+
+            for (int j = 1; j <=i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
 
 
 
