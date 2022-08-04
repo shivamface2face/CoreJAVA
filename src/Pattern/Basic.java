d235d9c41a81d73f21e90b17c50124bf39aa0bd7
shivamface2face <shivamfaceuit@gmail.com>
2022-08-04 08:37:10 +0530

    pattern pyramid

--- src/Pattern/Basic.java
+++ src/Pattern/Basic.java
@@ -4,6 +4,8 @@ public class Basic {
     public static void main(String[] args) {
         int n=4;
         int m=5;
+        System.out.println();
+        System.out.println();
 
 //        1 solid triangle
 
@@ -39,6 +41,29 @@ public class Basic {
 // 3rd type *
 //          * *
 //          * * *
+
+        for (int i = 1; i<=n; i++) {
+            for (int j = 1; j<=i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println();
+
+//        4 th type reverse pyramid
+//   * * *
+//   * *
+//   *
+
+        for (int i = n; i>=1; i--) {
+            for (int j = 1; j<=i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
 
 
 
