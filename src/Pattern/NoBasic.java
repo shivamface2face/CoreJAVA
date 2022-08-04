47912fb76db1117e3827d6971571695da09a6c88
shivamface2face <shivamfaceuit@gmail.com>
2022-08-04 12:01:43 +0530

    pattern of 0 1 triangle

--- src/Pattern/NoBasic.java
+++ src/Pattern/NoBasic.java
@@ -52,7 +52,26 @@ public class NoBasic {
             }
             System.out.println();
         }
+        System.out.println();
+        System.out.println();
 
+
+//     3 01 triangle
+//        1
+//        0 1
+//        1 0 1
+
+        for (int i = 1; i <=n ; i++) {
+            for (int j = 1; j <=i ; j++) {
+                int sum=i+j;
+                if (sum%2==0){
+                    System.out.print("1"+" ");
+                }else {
+                    System.out.print("0"+" ");
+                }
+            }
+            System.out.println();
+        }
 
 
     }
