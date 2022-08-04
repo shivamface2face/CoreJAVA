c52e679a43f2c2cadf3b1ad3fbf839ec40a41514
shivamface2face <shivamfaceuit@gmail.com>
2022-08-04 11:22:21 +0530

    pattern of no

--- src/Pattern/NoBasic.java
+++ src/Pattern/NoBasic.java
@@ -21,6 +21,37 @@ public class NoBasic {
         System.out.println();
         System.out.println();
 
+//        2 inverted pyramid no pattern
+//           1 2 3 4
+//           1 2 3
+//           1 2
+//           1
+
+        for (int i = n; i>=1; i--) {
+            for (int j = 1; j <=i ; j++) {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println();
+
+//        3 flyod pattern
+
+//        1
+//        2 3
+//        4 5 6
+//        7 8 9 10
+
+        int num=1;
+
+        for (int i = 1; i <=n ; i++) {
+            for (int j = 1; j <=i ; j++) {
+                System.out.print(num+" ");
+                num++;
+            }
+            System.out.println();
+        }
 
 
 
